a116efbb5407cc079fee06790effe04dd9200473
reverendus <reverendus@invalid>
2018-05-28 20:27:12 -0400

    Unit test coverage improved

--- tests/test_flop.py
+++ tests/test_flop.py
@@ -32,8 +32,10 @@ class TestAuctionKeeperFlopper:
         self.web3 = Web3(EthereumTesterProvider())
         self.web3.eth.defaultAccount = self.web3.eth.accounts[0]
         self.our_address = Address(self.web3.eth.defaultAccount)
+        self.other_address = Address(self.web3.eth.accounts[2])
         self.dai = DSToken.deploy(self.web3, 'DAI')
         self.dai.mint(Wad.from_number(10000000)).transact()
+        self.dai.transfer(self.other_address, Wad.from_number(1000000)).transact()
         self.mkr = DSToken.deploy(self.web3, 'MKR')
         self.flopper = Flopper.deploy(self.web3, self.dai.address, self.mkr.address)
 
@@ -61,7 +63,29 @@ class TestAuctionKeeperFlopper:
         assert round(auction.bid / auction.lot, 2) == round(Wad.from_number(825.0), 2)
         assert self.mkr.balance_of(self.our_address) == Wad(0)
 
-    def test_should_deal_when_auction_ends(self):
+    def test_should_bid_even_if_there_is_already_a_bidder(self):
+        # given
+        keeper = AuctionKeeper(args=args(f"--eth-from {self.web3.eth.defaultAccount} "
+                                         f"--flopper {self.flopper.address}"), web3=self.web3)
+
+        # and
+        self.flopper.approve(directly(from_address=self.other_address))
+        self.flopper.approve(directly())
+        self.flopper.kick(Address(self.web3.eth.accounts[1]), Wad.from_number(2), Wad.from_number(10)).transact()
+        self.flopper.dent(1, Wad.from_number(1.5), Wad.from_number(10)).transact(from_address=self.other_address)
+        assert self.flopper.bids(self.flopper.kicks()).lot == Wad.from_number(1.5)
+
+        # when
+        keeper.drive(1, Wad.from_number(825.0))
+        # and
+        keeper.check_all_auctions()
+        # then
+        auction = self.flopper.bids(self.flopper.kicks())
+        assert auction.lot < Wad.from_number(1.5)
+        assert round(auction.bid / auction.lot, 2) == round(Wad.from_number(825.0), 2)
+        assert self.mkr.balance_of(self.our_address) == Wad(0)
+
+    def test_should_deal_when_we_won_the_auction(self):
         # given
         keeper = AuctionKeeper(args=args(f"--eth-from {self.web3.eth.defaultAccount} "
                                          f"--flopper {self.flopper.address}"), web3=self.web3)
@@ -85,3 +109,22 @@ class TestAuctionKeeperFlopper:
         keeper.check_all_auctions()
         # then
         assert self.mkr.balance_of(self.our_address) > Wad(0)
+
+    def test_should_not_deal_when_auction_finished_but_somebody_else_won(self):
+        # given
+        keeper = AuctionKeeper(args=args(f"--eth-from {self.web3.eth.defaultAccount} "
+                                         f"--flopper {self.flopper.address}"), web3=self.web3)
+
+        # and
+        self.flopper.approve(directly(from_address=self.other_address))
+        self.flopper.approve(directly())
+        self.flopper.kick(Address(self.web3.eth.accounts[1]), Wad.from_number(2), Wad.from_number(10)).transact()
+        self.flopper.dent(1, Wad.from_number(1.5), Wad.from_number(10)).transact(from_address=self.other_address)
+        assert self.flopper.bids(self.flopper.kicks()).lot == Wad.from_number(1.5)
+
+        # when
+        time_travel_by(self.web3, self.flopper.ttl() + 5)
+        # and
+        keeper.check_all_auctions()
+        # then
+        assert self.mkr.balance_of(self.our_address) == Wad(0)
