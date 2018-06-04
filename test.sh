ee22ab7c12596875e40918d2d4a231f16fc850d6
reverendus <reverendus@invalid>
2018-06-04 14:58:15 -0400

    Allowed passing extra parameters (like `-k`) to `./test.sh`

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-PYTHONPATH=$PYTHONPATH:./lib/pymaker py.test --cov=auction_keeper --cov-report=term --cov-append tests/
+PYTHONPATH=$PYTHONPATH:./lib/pymaker py.test --cov=auction_keeper --cov-report=term --cov-append tests/ $@
