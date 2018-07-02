04077d5a20d071f9820f3a01764dc78ccd82dfc7
reverendus <reverendus@invalid>
2018-07-02 14:31:07 +0200

    Made method static, argument renamed.

--- auction_keeper/main.py
+++ auction_keeper/main.py
@@ -150,12 +150,13 @@ class AuctionKeeper:
                     gas_price = UpdatableGasPrice(output.gas_price)
                     self._run_future(bid_transact.transact_async(gas_price=gas_price))
 
-    def _run_future(self, fff):
+    @staticmethod
+    def _run_future(future):
         def worker():
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
             try:
-                asyncio.get_event_loop().run_until_complete(fff)
+                asyncio.get_event_loop().run_until_complete(future)
             finally:
                 loop.close()
 
