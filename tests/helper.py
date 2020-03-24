399c98e9d61b704fff3c6bfb21781b8b85ebcea9
Ed Noepel <46749157+EdNoepel@users.noreply.github.com>
2020-03-24 09:08:49 -0400

    Cleaned up unit tests (#55)
    * Implemented tests to check transaction mocking facilities
    
    * Increasing timeout got TestTransactionMocking working
    
    * Implemented a test where tx replacement is explicitly performed; I want to get this working under a variety of use cases, not just the one currently implemented
    
    * Trying to repair async tx mocking
    
    * Added an important wait to get the ignored tx in an appropriate state
    
    * Removed skips and sleeps from flap tests to see how Travis likes this
    
    * Reworked flip tests to reuse the keeper, removed all skips, fixed broken tests
    
    * Undid change to explictly prevent wait_for_other_threads while ignoring_transactions

--- tests/helper.py
+++ tests/helper.py
@@ -1,6 +1,6 @@
 # This file is part of Maker Keeper Framework.
 #
-# Copyright (C) 2018 reverendus
+# Copyright (C) 2018-2020 reverendus, EdNoepel
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -23,7 +23,11 @@ import time
 from contextlib import contextmanager
 from io import StringIO
 from mock import MagicMock
+from pymaker import Wad
 from web3 import Web3
+
+
+from pymaker import Receipt, Transact
 
 
 def args(arguments: str) -> list:
@@ -62,25 +66,66 @@ def wait_for_other_threads():
 
 
 class TransactionIgnoringTest:
+    class MockReceipt(Receipt):
+        def __init__(self):
+            super().__init__({
+                'transactionHash': '0xaaaaaaaaaabbbbbbbbbbccccccccccdddddddddd',
+                'gasUsed': 12345,
+                'logs': []
+            })
+            self.successful = True
+
     def start_ignoring_transactions(self):
+        """ Allows an async tx to be created and leaves it trapped in Transact's event loop """
         self.original_send_transaction = self.web3.eth.sendTransaction
         self.original_get_transaction = self.web3.eth.getTransaction
+        self.original_tx_count = self.web3.eth.getTransactionCount
         self.original_nonce = self.web3.eth.getTransactionCount(self.keeper_address.address)
 
         self.web3.eth.sendTransaction = MagicMock(return_value='0xaaaaaaaaaabbbbbbbbbbccccccccccdddddddddd')
         self.web3.eth.getTransaction = MagicMock(return_value={'nonce': self.original_nonce})
-        logging.debug("Started ignoring transactions")
+        self.web3.eth.getTransactionCount = MagicMock(return_value=0)
 
-    def end_ignoring_transactions(self):
-        # def second_send_transaction(transaction):
-        #     assert transaction['nonce'] == self.original_nonce
-        #
-        #     # TestRPC doesn't support `sendTransaction` calls with the `nonce` parameter
-        #     # (unlike proper Ethereum nodes which handle it very well)
-        #     transaction_without_nonce = {key: transaction[key] for key in transaction if key != 'nonce'}
-        #     return self.original_send_transaction(transaction_without_nonce)
-        #
-        # self.web3.eth.sendTransaction = MagicMock(side_effect=second_send_transaction)
-        self.web3.eth.sendTransaction = self.original_send_transaction
+        logging.debug(f"Started ignoring async transactions at nonce {self.original_nonce}")
+
+    def end_ignoring_transactions(self, ensure_next_tx_is_replacement=True):
+        """ Stops trapping an async tx, with a facility to ensure the next tx is a replacement (where desired) """
+        def second_send_transaction(transaction):
+            # Ensure the second transaction gets sent with the same nonce, replacing the first transaction.
+            assert transaction['nonce'] == self.original_nonce
+            # Restore original behavior for the third transaction.
+            self.web3.eth.sendTransaction = self.original_send_transaction
+
+            # TestRPC doesn't support `sendTransaction` calls with the `nonce` parameter
+            # (unlike proper Ethereum nodes which handle it very well)
+            transaction_without_nonce = {key: transaction[key] for key in transaction if key != 'nonce'}
+            return self.original_send_transaction(transaction_without_nonce)
+
+        # Give the previous Transact a chance to enter its event loop
+        time.sleep(0.1)
+
+        if ensure_next_tx_is_replacement:
+            self.web3.eth.sendTransaction = MagicMock(side_effect=second_send_transaction)
+        else:
+            self.web3.eth.sendTransaction = self.original_send_transaction
         self.web3.eth.getTransaction = self.original_get_transaction
-        logging.debug("Finished ignoring transactions")
+        self.web3.eth.getTransactionCount = self.original_tx_count
+
+        logging.debug("Finished ignoring async transactions")
+
+    def start_ignoring_sync_transactions(self):
+        """ Mocks submission of a tx, prentending it happened """
+        self.original_tx_count = self.web3.eth.getTransactionCount
+        self.original_get_receipt = Transact._get_receipt
+        self.original_func = Transact._func
+
+        Transact._get_receipt = MagicMock(return_value=TransactionIgnoringTest.MockReceipt())
+        Transact._func = MagicMock(return_value='0xccccccccccddddddddddaaaaaaaaaabbbbbbbbbb')
+        self.web3.eth.getTransactionCount = MagicMock(return_value=9999999999)
+        logging.debug("Started ignoring sync transactions")
+
+    def end_ignoring_sync_transactions(self):
+        self.web3.eth.getTransactionCount = self.original_tx_count
+        Transact._get_receipt = self.original_get_receipt
+        Transact._func = self.original_func
+        logging.debug("Finished ignoring sync transactions")
