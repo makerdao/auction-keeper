a878aaa18c644fd900108a480a596f9d28790aef
Ed Noepel <ed@makerdao.com>
2020-07-23 15:11:47 -0400

    change default so stdout doesn't show it checking auction 1 to 0

--- auction_keeper/main.py
+++ auction_keeper/main.py
@@ -71,7 +71,7 @@ class AuctionKeeper:
         parser.add_argument('--deal-for', type=str, nargs="+",
                             help="List of addresses for which auctions will be dealt")
 
-        parser.add_argument('--min-auction', type=int, default=1,
+        parser.add_argument('--min-auction', type=int, default=0,
                             help="Lowest auction id to consider")
         parser.add_argument('--max-auctions', type=int, default=1000,
                             help="Maximum number of auctions to simultaneously interact with, "
