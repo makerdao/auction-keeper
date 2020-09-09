327b4c04c2fa721be201ff641ffa78a24565cab1
Ed Noepel <ed@makerdao.com>
2020-09-09 10:19:39 -0400

    bugfix

--- tests/random_bid.py
+++ tests/random_bid.py
@@ -7,8 +7,7 @@ max_price = float(sys.argv[1]) if len(sys.argv) > 1 else 1000
 
 for line in sys.stdin:
     signal = json.loads(line)
-    auction_id = signal['id']
-    assert isinstance(auction_id, int)
+    auction_id = int(signal['id'])
 
     random.seed(a=auction_id)
     price = max_price * random.random()
