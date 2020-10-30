b1888048124be1c76f3bde5ae801b434d3e06d24
bkellerman <bert@ciphertrace.com>
2020-10-30 10:00:12 -0700

    upgrade requests version

--- install.sh
+++ install.sh
@@ -9,4 +9,8 @@ source _virtualenv/bin/activate
 # The advantage of using this method, in contrary to just calling `pip install -r requirements.txt` several times,
 # is that it can detect different versions of the same dependency and fail with a "Double requirement given"
 # error message.
-pip install $(cat requirements.txt $(find lib -name requirements.txt | sort) | sort | uniq | sed 's/ *== */==/g')
+#pip install $(cat requirements.txt $(find lib -name requirements.txt | sort) | sort | uniq | sed 's/ *== */==/g')
+pip install -r lib/pyflex/requirements.txt
+pip install -r lib/pygasprice-client/requirements.txt
+pip install -r requirements.txt
+
